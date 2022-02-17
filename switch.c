02c3976d56e611171cb6a602a03213a2d0ba7a2f
VaibhavDas2002 <98703459+VaibhavDas2002@users.noreply.github.com>
2022-02-17 12:29:57 +0530

    Conditional Statement project 2 (Switch Case)

---
+++ switch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    double n1, n2;
+
+    printf("Enter your choice: ");
+    scanf("%c", &op);
+
+    switch(op)
+    {
+        case '1':
+            printf("Food item - Pizza \n Price - Rs 239");
+            break;
+
+        case '2':
+            printf("Food item - Burger \n Price - Rs 129");
+            break;
+
+        case '3':
+            printf("Food item - Pasta \n Price - Rs 179");
+            break;
+
+        case '4':
+            printf("Food item - French Fries \n Price - Rs 99");
+            break;
+            
+        case '5':
+            printf("Food item - Sandwich \n Price - Rs 149");
+            break;
+
+        default:
+            printf("INVALID CHOICE!!");
+    }
+
+    return 0;
+}
